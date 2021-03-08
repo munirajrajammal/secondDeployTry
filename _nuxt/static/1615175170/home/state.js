aca82896abcc7f1e3e366720b03827bdcde3faba
munirajrajammal <muniraj.rajammal@gmail.com>
2021-03-08 09:25:37 +0530

    the commit from deploy try

---
+++ _nuxt/static/1615175170/home/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FsecondDeployTry\u002F_nuxt\u002Fstatic\u002F1615175170",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome",config:{app:{basePath:"\u002FsecondDeployTry\u002F",assetsPath:"\u002FsecondDeployTry\u002F_nuxt\u002F",cdnURL:a}}}}(null));
